fcb81ddda747ec4f3bd6330e4e09e74510132c0e
krnl0138 <krnl03182@gmail.com>
2022-01-24 04:22:52 +0300

    resolve after merge w/ responsive-css

--- src/components/burger-ingredients/card/burger-ingredients-card.tsx
+++ src/components/burger-ingredients/card/burger-ingredients-card.tsx
@@ -9,11 +9,7 @@ type TCardProps = {
   item: TIngredient;
   openDetails: () => void;
   cyData: number;
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 6758ee54fe1c91a3e584b76cc5d1b680752be022
 
 export default function Card({ item, openDetails, cyData }: TCardProps): JSX.Element {
   const cartIngredients = useSelector(store => store.cart.cartIngredients);
@@ -41,19 +37,11 @@ export default function Card({ item, openDetails, cyData }: TCardProps): JSX.Ele
       key={item._id}
       style={{ opacity }}
       data-cy={
-<<<<<<< HEAD
         item.type === 'bun'
           ? `card-bun-${cyData}`
           : item.type === 'main'
           ? `card-main-${cyData}`
           : `card-sauce-${cyData}`
-=======
-        item.type === 'bun' ? 
-          `card-bun-${cyData}` : 
-          item.type === 'main' ? 
-          `card-main-${cyData}` : 
-          `card-sauce-${cyData}`
->>>>>>> 6758ee54fe1c91a3e584b76cc5d1b680752be022
       }
     >
       {counter > 0 && (
