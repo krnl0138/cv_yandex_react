53e7efd326e5a4bb777ed08a373326b04c49139d
krnl0138 <krnl03182@gmail.com>
2022-01-25 22:04:03 +0300

    rewrite app-header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,15 +1,10 @@
 import styles from './app-header.module.scss';
 import React from 'react';
-import {
-  Logo,
-  BurgerIcon,
-  ListIcon,
-  ProfileIcon,
-} from '@ya.praktikum/react-developer-burger-ui-components';
-import { Link, NavLink } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import { AppHeaderMobile } from './app-header-mobile/app-header-mobile';
+import { AppHeaderDesktop } from './app-header-desktop/app-header-desktop';
 import { AppHeaderSmall } from './app-header-small/app-header-small';
+import { AppHeaderMobile } from './app-header-mobile/app-header-mobile';
+import { AppHeaderHamburger } from './F-app-header-hamburger/app-header-hamburger';
 import { DESKTOP_SCREEN_SIZE, MOBILE_SCREEN_SIZE } from '../../utils/constants';
 
 export default function AppHeader(): JSX.Element {
@@ -20,54 +15,12 @@ export default function AppHeader(): JSX.Element {
   });
   const isMobileScreen = useMediaQuery({ maxDeviceWidth: MOBILE_SCREEN_SIZE });
 
-  const AppHeaderDesktop = () => {
-    return (
-      <>
-        <NavLink
-          exact
-          to={{ pathname: '/' }}
-          activeClassName={styles.activeLink}
-          className={`${styles.nav_element} pt-2 pb-2 pl-5 pr-5 ${styles.link}`}
-        >
-          <BurgerIcon type="secondary" />
-          <p className={`${styles.app_header_button} ml-2`}>Конструктор</p>
-        </NavLink>
-
-        <NavLink
-          to={{ pathname: '/feed' }}
-          activeClassName={styles.activeLink}
-          className={` pt-2 pb-2 pl-5 pr-5 ${styles.nav_element} ${styles.link}`}
-          data-cy="feed-page-link"
-        >
-          <ListIcon type="secondary" />
-          <p className={`${styles.app_header_button} ml-2`}>Лента заказов</p>
-        </NavLink>
-
-        <span className={styles.center}>
-          <Link to={{ pathname: '/' }}>
-            <Logo />
-          </Link>
-        </span>
-
-        <NavLink
-          to={{ pathname: '/profile' }}
-          activeClassName={styles.activeLink}
-          className={` pt-2 pb-2 pl-5 pr-5 ${styles.nav_element} ${styles.nav_element_right} ${styles.link}`}
-        >
-          <ProfileIcon type="secondary" />
-          <p className={`${styles.app_header_button} ml-2`}>Профиль</p>
-        </NavLink>
-      </>
-    );
-  };
-
+  // {/* {isMobileScreen && <AppHeaderMobile />} */}
   return (
-    <header className={`${styles.header} text text_type_main-small text_color_inactive `}>
-      <nav className={styles.nav}>
-        {isMobileScreen && <AppHeaderMobile />}
-        {isSmallScreen && <AppHeaderSmall />}
-        {isDesktopScreen && <AppHeaderDesktop />}
-      </nav>
-    </header>
+    <>
+      {isMobileScreen && <AppHeaderHamburger />}
+      {isSmallScreen && <AppHeaderSmall />}
+      {isDesktopScreen && <AppHeaderDesktop />}
+    </>
   );
 }
