3a500a1f44bc60247075f05bc13ead5b8dea2cde
krnl0138 <krnl03182@gmail.com>
2022-01-26 04:00:38 +0300

    rework mobile version

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,26 +1,54 @@
 import styles from './app-header.module.scss';
 import React from 'react';
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Link, NavLink } from 'react-router-dom';
+import { SMALL_SCREEN_SIZE, MOBILE_SCREEN_SIZE } from '../../utils/constants';
 import { useMediaQuery } from 'react-responsive';
-import { AppHeaderDesktop } from './app-header-desktop/app-header-desktop';
-import { AppHeaderSmall } from './app-header-small/app-header-small';
-import { AppHeaderMobile } from './app-header-mobile/app-header-mobile';
-import { AppHeaderHamburger } from './F-app-header-hamburger/app-header-hamburger';
-import { DESKTOP_SCREEN_SIZE, MOBILE_SCREEN_SIZE } from '../../utils/constants';
+import { AppHeaderHamburger } from './app-header-hamburger/app-header-hamburger';
+import { CustomLink } from './app-header-custom-link/app-header-custom-link';
 
 export default function AppHeader(): JSX.Element {
-  const isDesktopScreen = useMediaQuery({ minDeviceWidth: DESKTOP_SCREEN_SIZE });
+  const isMobileScreen = useMediaQuery({ maxDeviceWidth: MOBILE_SCREEN_SIZE });
   const isSmallScreen = useMediaQuery({
-    maxDeviceWidth: DESKTOP_SCREEN_SIZE - 1,
+    maxDeviceWidth: SMALL_SCREEN_SIZE - 1,
     minDeviceWidth: MOBILE_SCREEN_SIZE + 1,
   });
-  const isMobileScreen = useMediaQuery({ maxDeviceWidth: MOBILE_SCREEN_SIZE });
+  const isDesktopScreen = useMediaQuery({ minDeviceWidth: SMALL_SCREEN_SIZE });
 
-  // {/* {isMobileScreen && <AppHeaderMobile />} */}
   return (
-    <>
-      {isMobileScreen && <AppHeaderHamburger />}
-      {isSmallScreen && <AppHeaderSmall />}
-      {isDesktopScreen && <AppHeaderDesktop />}
-    </>
+    <header className={`${styles.header} text text_type_main-small text_color_inactive `}>
+      <nav className={styles.nav}>
+        {isDesktopScreen && (
+          <>
+            <CustomLink url="/" icon="burger" text="Конструктор" right={false} />
+            <CustomLink url="/feed" icon="list" text="Лента заказов" right={false} />
+
+            <Link to={{ pathname: '/' }} className={styles.logo}>
+              <Logo />
+            </Link>
+
+            <CustomLink url="/profile" icon="profile" text="Профиль" right={true} />
+          </>
+        )}
+
+        {isSmallScreen && (
+          <>
+            <CustomLink url="/" icon="burger" text="" right={false} />
+            <CustomLink url="/feed" icon="list" text="" right={false} />
+            <CustomLink url="/profile" icon="profile" text="" right={false} />
+            <Link to={{ pathname: '/' }} className={styles.pushRight}>
+              <Logo />
+            </Link>
+          </>
+        )}
+
+        {isMobileScreen && <AppHeaderHamburger />}
+      </nav>
+    </header>
   );
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const DESKTOP_SCREEN_SIZE = 1025;
-export const SMALL_SCREEN_SIZE = 1024;
-export const MOBILE_SCREEN_SIZE = 800;
+export const DESKTOP_SCREEN_SIZE = 1200;
+export const MEDIUM_SCREEN_SIZE = 1000;
+export const SMALL_SCREEN_SIZE = 700;
+export const MOBILE_SCREEN_SIZE = 400;
